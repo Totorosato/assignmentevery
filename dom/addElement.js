acca243f265109a942cc7c356b664f5adf8461df
Bumanhuslen Erdenebaatar <23LP6507@G304-CC0062.local>
2023-11-16 19:32:42 +0800

    16

--- dom/addElement.js
+++ dom/addElement.js
@@ -1,20 +1,32 @@
-document.addEventListener(
-  "keydown",
-  (event) => {
-    const keyName = event.key;
+rndNumber1 = Math.floor(Math.random() * 100);
+rndNumber2 = Math.floor(Math.random() * 100);
+document.getElementById("randomNumber1").value = rndNumber1;
+document.getElementById("randomNumber2").value = rndNumber2;
+function check() {
+  var sum = rndNumber1 + rndNumber2;
+  const check = document.getElementById("result").value;
+  if (sum === check) {
+    alert("sain golog ve zov bna ");
+  } else {
+    alert("suga bandi ve ");
+  }
+}
+function reset() {
+  rndNumber1 = Math.floor(Math.random() * 100);
+  rndNumber2 = Math.floor(Math.random() * 100);
+  document.getElementById("randomNumber1").value = rndNumber1;
+  document.getElementById("randomNumber2").value = rndNumber2;
+}
 
-    if (keyName === "Control") {
-      // do not alert when only Control key is pressed.
-      return;
-    }
 
-    if (event.ctrlKey) {
-      // Even though event.key is not 'Control' (e.g., 'a' is pressed),
-      // event.ctrlKey may be true if Ctrl key is pressed at the same time.
-      alert(`Combination of ctrlKey + ${keyName}`);
-    } else {
-      alert(`Key pressed ${keyName}`);
-    }
-  },
-  false
-);
+
+
+function () {
+  for (let i = 0 ; i<10 ; i++) {
+    var createCircle = document.createElement("div");
+  document.appendChild(createCircle);
+  createCircle.classList.add("circle")
+  
+}
+  document.getElementsByClassName("circle") = array ;
+}
